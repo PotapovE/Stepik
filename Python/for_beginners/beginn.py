cc0b22227e2223b171df26a72d9e962c97447ddb
PotapovE <potapovomsk@gmail.com>
2022-03-03 12:12:02 +0300

    Решение задач на Stepik 4.3.11

--- Python/for_beginners/beginn.py
+++ Python/for_beginners/beginn.py
@@ -178,3 +178,32 @@ if 0 <= a <= 36:
             print('черный')
 else:
     print('ошибка ввода')
+
+# 4.3.11 На числовой прямой даны два отрезка. Напишите программу, которая находит их пересечение.
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+if a2 > b1 or b2 < a1:
+    print('пустое множество')
+elif a2 == b1:
+    print(a2)
+elif a1 >= a2:
+    if b1 > b2:
+        if a1 == b2:
+            print(a1)
+        else:
+            print(a1, b2)
+    else:
+        if a1 == b1:
+            print(a1)
+        else:
+            print(a1, b1)
+elif a1 < a2:
+    if b1 > b2:
+        if a2 == b2:
+            print(a2)
+        else:
+            print(a2, b2)
+    else:
+        if a2 == b1:
+            print(a2)
+        else:
+            print(a2, b1)
