c8475a4d1877d51bb5551b9f29b6d77c5c100ae3
PotapovE <potapovomsk@gmail.com>
2022-03-08 22:55:58 +0300

    Решение задач на Stepik 7.3.9

--- Python/for_beginners/beginn.py
+++ Python/for_beginners/beginn.py
@@ -470,3 +470,10 @@ for i in range(1, n + 1):
     if i * i % 10 == 5:
         total += i
 print(total)
+
+# 7.3.9 На вход программе подается натуральное число n. Напишите программу, которая вычисляет n!.
+n = int(input())
+total = 1
+for i in range(1, n + 1):
+    total *= i
+print(total)
