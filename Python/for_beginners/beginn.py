1ecefd5021292d808ad8399d7127c6082c3cb6d6
PotapovE <potapovomsk@gmail.com>
2022-03-08 23:14:21 +0300

    Решение задач на Stepik 7.3.11

--- Python/for_beginners/beginn.py
+++ Python/for_beginners/beginn.py
@@ -485,3 +485,7 @@ for _ in range(10):
     if num: 
         total *= num
 print(total)
+
+# 7.3.11 На вход программе подается натуральное число n. Напишите программу, которая вычисляет сумму всех его делителей.
+n = int(input())
+print(sum([i for i in range(1, n + 1) if n % i == 0]))
