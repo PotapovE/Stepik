5af4fcbdcbd4f6b86ba678f8ecbdac6e25e41b3f
PotapovE <potapovomsk@gmail.com>
2022-03-12 23:03:58 +0300

    Решение задач на Stepik 7.8.9

--- Python/for_beginners/beginn.py
+++ Python/for_beginners/beginn.py
@@ -664,3 +664,10 @@ for i in range(1, n + 1):
     print('*' * i)
 for i in range(n - 1, 0, -1):
     print('*' * i)
+
+# 7.8.9 Дано натуральное число n. Напишите программу, которая печатает численный треугольник в соответствии с примером.
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(i, end='')
+    print()
