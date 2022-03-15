b9136e87b07381bdea3ad6e6a80a0335233b796f
PotapovE <potapovomsk@gmail.com>
2022-03-15 23:59:27 +0300

    Решение задач на Stepik 7.9.2

--- Python/for_beginners/beginn.py
+++ Python/for_beginners/beginn.py
@@ -680,3 +680,12 @@ for i in range(1, n + 1):
         counter += 1
         print(counter, end=' ')
     print()
+
+# 7.9.2 Дано натуральное число n. Напишите программу, которая печатает численный треугольник с высотой равной n.
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end='')
+    for k in range(i - 1, 0, -1):
+        print(k, end='')        
+    print()
