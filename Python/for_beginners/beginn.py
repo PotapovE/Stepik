8c1e8927d5052e5eb125c68802592bad8fc8d187
PotapovE <potapovomsk@gmail.com>
2022-03-17 00:12:05 +0300

    Решение задач на Stepik 7.9.6

--- Python/for_beginners/beginn.py
+++ Python/for_beginners/beginn.py
@@ -726,3 +726,13 @@ total = sum([int(i) for i in input()])
 while total > 9:
     total = sum([int(i) for i in str(total)])
 print(total) 
+
+# 7.9.6 Дано натуральное число n. Напишите программу, которая выводит значение суммы 1!+2!+3!+…+n!.
+total = 0
+factorial = 1
+for i in range(1, int(input()) + 1):
+    for j in range(1, i + 1):
+        factorial *= j   
+    total += factorial
+    factorial = 1
+print(total)
