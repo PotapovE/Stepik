cb6c3a21f0c1fb42644a0bcfacb9b67769828cf2
PotapovE <potapovomsk@gmail.com>
2022-03-22 22:56:20 +0300

    Решение задач на Stepik 11.4.2

--- Python/for_beginners/beginn.py
+++ Python/for_beginners/beginn.py
@@ -1002,3 +1002,7 @@ s = []
 for _ in range(int(input())):
     s.extend(input())
 print(s)
+
+# 11.4.2 Дополните приведенный код, так чтобы он вывел сумму квадратов элементов списка numbers.
+numbers = [1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111]
+print(sum([i * i for i in numbers]))
