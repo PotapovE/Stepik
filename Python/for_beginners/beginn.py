dc9c066093c5bb10c1598b70a4ef3eda0225489d
PotapovE <potapovomsk@gmail.com>
2022-04-08 23:08:49 +0300

    Решение задач на Stepik 13.5.6

--- Python/for_beginners/beginn.py
+++ Python/for_beginners/beginn.py
@@ -1225,3 +1225,15 @@ def is_password_good(password):
     return False
 txt = input()
 print(is_password_good(txt))
+
+# 13.5.6 Напишите функцию is_one_away(word1, word2), которая принимает в качестве аргументов два слова word1 и word2 
+# и возвращает значение True если слова имеют одинаковую длину и отличаются ровно в 1 символе и False в противном случае.
+def is_one_away(word1, word2):
+    if len(word1) == len(word2):
+        s = len([i for i in range(len(word1)) if word1[i] != word2[i]])
+        if s == 1:
+            return True
+    return False
+txt1 = input()
+txt2 = input()
+print(is_one_away(txt1, txt2))
