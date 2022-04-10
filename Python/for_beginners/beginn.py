63a8547f360c6d1fcf136ef1d580e3e272dd62bd
PotapovE <potapovomsk@gmail.com>
2022-04-10 11:44:36 +0300

    Решение задач на Stepik 13.6.5

--- Python/for_beginners/beginn.py
+++ Python/for_beginners/beginn.py
@@ -1320,3 +1320,23 @@ r = float(input())
 # вызываем функцию
 length, square = get_circle(r)
 print(length, square)
+
+# 13.6.5 Hапишите функцию solve(a, b, c), которая принимает в качестве аргументов три целых числа a, b, c 
+# – коэффициенты квадратного уравнения ax^2+bx+c = 0 и возвращает его корни в порядке возрастания.
+# объявление функции
+def solve(a, b, c):
+    disc = (b ** 2) - (4 * a * c)
+    if disc >= 0:
+        if disc == 0:
+            x = (- b) / (2 * a)
+            return x, x
+        else:
+            x1 = (- b - disc ** 0.5) / (2 * a)
+            x2 = (- b + disc ** 0.5) / (2 * a)
+            return min(x1, x2), max(x1, x2)     
+# считываем данные
+a, b, c = int(input()), int(input()), int(input())
+
+# вызываем функцию
+x1, x2 = solve(a, b, c)
+print(x1, x2)
