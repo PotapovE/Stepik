042df9b1d5f824c1c4c398bc36266f081a011a5a
PotapovE <potapovomsk@gmail.com>
2022-05-01 11:41:20 +0300

    елочка на C#

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,21 +1,9 @@
-﻿// Сортировка пузырьком
+﻿// Елочка
 
-int[] num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-// int[] num = new int [line.Length];
-// for (int i = 0; i < line.Length; i++)
-// {
-//     num[i] = Convert.ToInt32(line[i]);
-// }
-for (int i = 0; i < num.Length - 1; i++)
+int height = int.Parse(Console.ReadLine());
+for (int i = 1; i <= height; ++i)
 {
-    for (int j = 0; j < num.Length - 1; j++)
-    {
-        if (num[j] > num[j + 1])
-        {
-            int temp = num[j];
-            num[j] = num[j + 1];
-            num[j + 1] = temp;
-        }
-    }
+    string spaces = new string(' ', height - i);
+    string mirrorPart = new string ('*', i - 1);
+    Console.WriteLine($"{spaces}{mirrorPart}*{mirrorPart}");
 }
-Console.WriteLine(String.Join(' ', num));
