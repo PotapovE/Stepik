8ae2a9e7d0e8de03b1b41ab37a56e9592680b9b5
PotapovE <potapovomsk@gmail.com>
2022-05-02 19:32:29 +0300

    шелчок часов на 30 градусов минутной стрелки от часовой на C#

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,9 +1,14 @@
-﻿// Елочка
+﻿// Часы
 
-int height = int.Parse(Console.ReadLine());
-for (int i = 1; i <= height; ++i)
+int n = int.Parse(Console.ReadLine());
+int hour = n % 12;
+int min = hour * 5 + 5;
+int dop = min / 12;
+int find = min + dop;
+if (find != dop) find += 1;
+if (find >= 60) 
 {
-    string spaces = new string(' ', height - i);
-    string mirrorPart = new string ('*', i - 1);
-    Console.WriteLine($"{spaces}{mirrorPart}*{mirrorPart}");
+   find -= 60; 
+   hour = (hour + 1) % 12;
 }
+Console.WriteLine(find < 10 ? $"{hour}:0{find}": $"{hour}:{find}");
